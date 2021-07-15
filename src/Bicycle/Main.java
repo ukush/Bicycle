55939e5a221be25be9448a3f0bcaaa0a9884c973
ukush <64285005+ukush@users.noreply.github.com>
2021-07-15 15:33:21 +0100

    added material and colour choices

--- src/Bicycle/Main.java
+++ src/Bicycle/Main.java
@@ -1,5 +1,4 @@
 package Bicycle;
-
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -23,28 +22,49 @@ public class Main {
 
     public static void main(String[] args) {
 
-        welcome();
-        System.out.println("Choose which type of bike you want to create.");
+        welcome(); // Introduce user to the application
 
-        for (int i = 0; i<2; i++) {
-            System.out.println(( i+1) + ". " + bikeType[i] + " bike");
-        }
-
-
-        int typeChoice = keyboard.nextInt();
+        System.out.println("Choose which type of bike you want.");
+        printTypes(); // print out the types of bikes the user can choose from
+        int typeChoice = keyboard.nextInt(); // store the user's choice
 
         if (typeChoice == 1) {
-            //they chose road bike
-            System.out.println("Ah so you like cycling on the roads?");
+            //if the user chooses road bike
+            System.out.println("Are you ready for some serious speeds?");
         }
         else {
-            System.out.println("Adventurous are we?");
+            System.out.println("Nice, You must be the adventurous type!");
         }
-        System.out.println("Now you need to decide which material you want your bike to made out of. We offer a few different choices:");
-        for (int i = 0; i<4; i++) {
-            System.out.println(( i+1) + ". " + bikeMaterials[i]);
-        }
-        int materialChoice = keyboard.nextInt();
+
+        System.out.println("Now you need to decide which material you want your bike to be made out of. We offer a few different choices:");
+        printMaterialList(typeChoice); //show the user the list of materials they can choose from
+        int materialChoice = keyboard.nextInt(); // store choice
+
+        System.out.println("Select a colour:");
+        printColours();
+        int colourChoice = keyboard.nextInt();
+
+        /*
+         * The weight of the bike depends on the frame material and the size of the wheels
+         * carbon fibre => 9.0kg
+         * Titanium => 11.6kg
+         * steel => 12.2kg
+         * aluminium => 10.7kg
+         *
+         * for road bikes:
+         * 22mm tyres add 0.6kg
+         * 25mm tyres add 0.8kg
+         * 28mm tyres add 1.0kg
+         *
+         *
+         */
+
+
+        System.out.println();
+
+
+
+
 
 
 
@@ -54,11 +74,49 @@ public class Main {
     public static void welcome(){
         //application introduction
         System.out.println();
-        System.out.println("Welcome to the interactive bicycle simulator");
+        System.out.println("Welcome to Atom Cycle Centre");
         System.out.println("In this application, you will be able to create your own custom bicycle " +
                 "and take it for a virtual ride (you'll have to use your imagination).");
         System.out.println();
-        System.out.println("Let's start off by creating your own custom bike.");
+        System.out.println("Let's make sure we get you the best bike for you.");
+    }
+
+    public static void printTypes(){
+        for (int i = 0; i<2; i++) {
+            System.out.println(( i+1) + ". " + bikeType[i] + " bike");
+        }
+
+    }
+
+    public static void printMaterialList(int type){
+        for (int i = 0; i<4; i++) {
+            System.out.print(( i+1) + ". " + bikeMaterials[i]);
+
+            if (type == 1) {
+                if (i ==0) {
+                    System.out.println("(recommended)");
+                }
+                else {
+                    System.out.println();
+                }
+
+            }
+            else {
+                if (i == 3) {
+                    System.out.println("(recommended)");
+                }
+                else {
+                    System.out.println();
+                }
+            }
+
+        }
+    }
+
+    public static void printColours(){
+        for (int i=0; i<5; i++) {
+            System.out.print(( i+1) + ". " + bikeColours[i]);
+        }
     }
 
 

206abe659fdfdf531cc6a613f938e952bd463b4c
ukush <64285005+ukush@users.noreply.github.com>
2021-07-30 12:58:41 +0100

    create bicycle object completed

--- src/Bicycle/Bicycle.java
+++ src/Bicycle/Bicycle.java
@@ -2,13 +2,6 @@ package Bicycle;
 
 public class Bicycle {
 
-    /**
-     * Declare the states that bicycles of all types will share
-     * Declare the accessor methods to these states
-     * Declare a constructor method?
-     * Write the methods that all bicycles will share
-     */
-
     //physical characteristics of a bicycle
 
     private String name;
@@ -16,13 +9,11 @@ public class Bicycle {
     private String material;
     private String colour;
     private float weight;
-    private int tyreDiameter;
+    private float tyreDiameter;
 
     //other attributes
     private int topSpeed;
-    private int currentSpeed;
     private int totalGears;
-    private int currentGear;
     private double price;
 
 
@@ -54,12 +45,13 @@ public class Bicycle {
         this.weight = newWeight;
     }
 
-    public int getTyreDiameter(){
+    public float getTyreDiameter(){
         return tyreDiameter;
     }
-    public void setTyreDiameter(int newDiameter){
+    public void setTyreDiameter(float newDiameter){
         this.tyreDiameter = newDiameter;
     }
+
 
     public int getTopSpeed(){
         return topSpeed;
@@ -68,25 +60,11 @@ public class Bicycle {
         this.topSpeed = newTopSpeed;
     }
 
-    public int getCurrentSpeed(){
-        return currentSpeed;
-    }
-    public void setCurrentSpeed(int newSpeed){
-        this.currentSpeed = newSpeed;
-    }
-
     public int getTotalGears(){
         return totalGears;
     }
     public void setTotalGears(int newTotalGears){
         this.totalGears = newTotalGears;
-    }
-
-    public int getCurrentGear(){
-        return currentGear;
-    }
-    public void setCurrentGear(int newGear){
-        this.currentGear = newGear;
     }
 
     public double getPrice(){
@@ -101,7 +79,7 @@ public class Bicycle {
 
     public Bicycle(){}  //define empty constructor
 
-    public Bicycle(String theName, String theType, String theMaterial, String theColour, float theWeight, int theTyreDiameter, int theTopSpeed, int theTotalGears, double thePrice){
+    public Bicycle(String theName, String theType, String theMaterial, String theColour, float theWeight, float theTyreDiameter, int theTopSpeed, int theTotalGears, double thePrice){
 
         this.name = theName;
         this.type = theType;
@@ -117,48 +95,16 @@ public class Bicycle {
 
     // Methods for all bike objects
 
-    public int accelerate() {
-        // will increase the speed by 5mph
-
-        int speed = getCurrentSpeed();
-        setCurrentSpeed(speed + 5);
-        return currentSpeed;
-    }
-
-    public int slowDown(){
-        // will decrease the speed by 5mph
-        int speed = getCurrentSpeed();
-        setCurrentSpeed(speed - 5);
-        return currentSpeed;
-    }
-
-    public int gearUp() {
-        // will shift one gear up (until you reach the highest gear which depends on the bike)
-        // Mountain bikes have 7 gears and road bikes have 16 gears
-
-        int gear = getCurrentGear();
-        setCurrentGear(gear + 1);
-        return currentGear;
-
-    }
-
-    public int gearDown() {
-        // will shift one gear down (until you reach the lowest gear which is 1)
-
-        int gear = getCurrentGear();
-        setCurrentGear(gear - 1);
-        return currentGear;
-    }
-
     public void showSpecs(){
         System.out.println("Model Name: " + getName());
         System.out.println("Bicycle Type : " + getType());
         System.out.println("Material : " + getMaterial());
         System.out.println("Colour : " + getColour());
         System.out.println("Weight : " + getWeight() + "kg");
-        System.out.println("Standard Tyre Diameter : " + getTyreDiameter() + "mm");
+        System.out.println("Tyre Diameter : " + getTyreDiameter() + "\"");
         System.out.println("Top Speed : " + getTopSpeed() + "mph");
         System.out.println("Number of Gears : " + getTotalGears());
+        System.out.println("Price: £" + getPrice());
     }
 
 
--- src/Bicycle/Main.java
+++ src/Bicycle/Main.java
@@ -1,99 +1,122 @@
 package Bicycle;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main {
 
-    //set up scanner
-    public static Scanner keyboard = new Scanner(System.in);
+    public static HashMap<Integer, Integer> customPrices = new HashMap<>();
 
-    public static String[] bikeType = new String[]{ "Road", "Mountain"};
-    public static String[] bikeColours = new String[]{ "Matt Black", "Matt Grey", "Racing Red", "Volt Blue", "Silver", "Gloss Black"};
-    public static String[] bikeMaterials = new String[]{"Carbon fibre", "Titanium", "Steel", "Aluminium"};
-    public static int[] tyreDiameters = new int[]{22,25,28};
+    private static final String[] types = {"road", "mountain"};
+    private static final String[] materials = {"aluminium", "steel", "titanium", "carbon fibre"};
+    private static final String[] colours = {"racing red", "volt blue", "gunmetal grey", "silver", "matte black"};
+    private static final float[] roadTyres = {22f, 25f, 28f};
+    private static final float[] mountainTyres = {26f, 27.5f, 29f};
+    private static final int [] prices = {119, 99, 129, 0, 179, 229, 35, 35, 50, 0, 50, 19, 0, 29, 29, 35, 39, 45, 55, 65, 85};
+    private static final int[] gears = {7,12,16,21};
 
-
-    // List of bicycle models
-    // Separately store road bikes and mountain bikes
-    ArrayList<Bicycle> mountainBikes = new ArrayList<>();
-    ArrayList<Bicycle> roadBikes = new ArrayList<>();
-
+    private static final Scanner input = new Scanner(System.in);
 
 
     public static void main(String[] args) {
 
-        welcome(); // Introduce user to the application
-
-        System.out.println("Choose which type of bike you want.");
-        printTypes(); // print out the types of bikes the user can choose from
-        int typeChoice = keyboard.nextInt(); // store the user's choice
-
-        if (typeChoice == 1) {
-            //if the user chooses road bike
-            System.out.println("Are you ready for some serious speeds?");
-        }
-        else {
-            System.out.println("Nice, You must be the adventurous type!");
+        //Map all keys to values using hashmap
+        for (int i = 0; i < prices.length; i++){
+            customPrices.put((i+1), prices[i]);
         }
 
-        System.out.println("Now you need to decide which material you want your bike to be made out of. We offer a few different choices:");
-        printMaterialList(typeChoice); //show the user the list of materials they can choose from
-        int materialChoice = keyboard.nextInt(); // store choice
 
-        System.out.println("Select a colour:");
-        printColours();
-        int colourChoice = keyboard.nextInt();
+        // introduce the program to the user and display the main menu
+        welcome(); // Introduce application to user
+        displayMenu();
+        int choice = validateMainMenuInput(); // display options to user and assign the return value to the variable choice.
 
-        /*
-         * The weight of the bike depends on the frame material and the size of the wheels
-         * carbon fibre => 9.0kg
-         * Titanium => 11.6kg
-         * steel => 12.2kg
-         * aluminium => 10.7kg
-         *
-         * for road bikes:
-         * 22mm tyres add 0.6kg
-         * 25mm tyres add 0.8kg
-         * 28mm tyres add 1.0kg
-         *
-         *
-         */
-
-
-        System.out.println();
-
-
-
-
-
-
-
-
+        switch (choice) {
+            case 0: break;
+            case 1:{
+                viewBicycleMenu();
+                int viewChoice = validateInput(2); //display menu and validate input and set return value of choice to viewChoice variable
+                if (viewChoice ==1){
+                    System.out.println("Run method that prints out all names of bicycles in a list");
+                }
+                else { // because the input has already been validated, if it is not 1 it can only be 2 therefore no need for else-if etc.
+                    System.out.println("Enter the name of the bicycle: ");
+                }
+                break;
+            }
+            case 2: {
+               Bicycle newBike = createNewBicycle();
+                System.out.println();
+                newBike.showSpecs();
+                break;
+            }
+            case 3: {
+                System.out.println("Enter the name of the bicycle you want to remove");
+                break;
+            }
+        }
     }
-
     public static void welcome(){
         //application introduction
         System.out.println();
         System.out.println("Welcome to Atom Cycle Centre");
-        System.out.println("In this application, you will be able to create your own custom bicycle " +
-                "and take it for a virtual ride (you'll have to use your imagination).");
-        System.out.println();
-        System.out.println("Let's make sure we get you the best bike for you.");
+        System.out.println("Select from the options below: ");
     }
 
-    public static void printTypes(){
-        for (int i = 0; i<2; i++) {
-            System.out.println(( i+1) + ". " + bikeType[i] + " bike");
-        }
-
+    public static void displayMenu() {
+        System.out.println("1. View Bicycles");
+        System.out.println("2. Create Custom Bicycle");
+        System.out.println("3. Remove Bicycle from Store");
+        System.out.println("0. Exit");
     }
 
-    public static void printMaterialList(int type){
-        for (int i = 0; i<4; i++) {
-            System.out.print(( i+1) + ". " + bikeMaterials[i]);
+    public static void viewBicycleMenu(){
+        System.out.println("Would you like to:");
+        System.out.println("1. View all bicycles");
+        System.out.println("2. Search for bicycles by name");
+    }
 
-            if (type == 1) {
-                if (i ==0) {
+    public static int validateMainMenuInput(){
+
+        boolean isInputValid;
+        int choice = 0;
+        do {
+            try {
+                choice = input.nextInt(); // take in user input
+                // create loop to validate the input is within range
+                if (choice<0||choice>3) { // if the input is not in range
+                    System.out.println("Error: That is not a valid option. Please try again."); // display error message
+                    isInputValid = false; // trigger while loop
+                    input.nextLine(); // clear scanner
+                }
+                else {
+                    isInputValid = true; // stop while loop
+                }
+            } catch (InputMismatchException e) { //catch exception of input is not a number (InputMismatch exception)
+                input.nextLine(); // clear scanner token
+                System.out.println("That is not a valid input. Please enter a number using your keypad."); // display error message
+                isInputValid = false; // trigger the while loop
+            }
+        }while(!isInputValid);
+        return choice;
+    }
+
+
+    public static int getPrices(int userInput){
+        return customPrices.get(userInput);
+    }
+
+    public static int addPrices(int price1, int price2, int price3, int price4, int price5){
+        return price1 + price2 + price3 + price4 +price5;
+    }
+
+    public static void printMaterials(int bikeType, String [] array){
+        for (int i = 0; i< array.length; i++) {
+            System.out.print(( i+1) + ". " + array[i]);
+
+            if (bikeType == 1) {
+                if (i ==3) {
                     System.out.println("(recommended)");
                 }
                 else {
@@ -102,7 +125,7 @@ public class Main {
 
             }
             else {
-                if (i == 3) {
+                if (i == 0) {
                     System.out.println("(recommended)");
                 }
                 else {
@@ -113,11 +136,142 @@ public class Main {
         }
     }
 
-    public static void printColours(){
-        for (int i=0; i<5; i++) {
-            System.out.print(( i+1) + ". " + bikeColours[i]);
+    public static void printColours(String [] array){
+        for (int i=0; i< array.length; i++) {
+            System.out.print(( i+1) + ". " + array[i]);
+            System.out.println();
         }
     }
+
+    public static void printTyreDiameters(float [] array){
+        for (int i=0; i< array.length; i++) {
+            System.out.print(( i+1) + ". " + array[i] + "\"");
+            System.out.println();
+        }
+    }
+
+    public static void printTypes(String [] array){
+        for (int i = 0; i< array.length; i++) {
+            System.out.println(( i+1) + ". " + array[i] + " bike");
+        }
+
+    }
+
+    public static void printGears(int [] array){
+        for (int i = 0; i< array.length; i++) {
+            System.out.println(( i+1) + ". " + array[i]);
+        }
+
+    }
+
+    public static int validateInput(int numberOfOptions){
+
+        boolean isInputValid;
+        int choice = 0;
+        do {
+            try {
+                choice = input.nextInt(); // take in user input
+                // create loop to validate the input is within range
+                if (choice<=0||choice>numberOfOptions) { // if the input is not in range
+                    System.out.println("Error: That is not a valid option. Please try again."); // display error message
+                    isInputValid = false; // trigger while loop
+                    input.nextLine(); // clear scanner
+                }
+                else {
+                    isInputValid = true; // stop while loop
+                }
+            } catch (InputMismatchException e) { //catch exception of input is not a number (InputMismatch exception)
+                input.nextLine(); // clear scanner token
+                System.out.println("That is not a valid input. Please enter a number using your keypad."); // display error message
+                isInputValid = false; // trigger the while loop
+            }
+        }while(!isInputValid);
+        return choice;
+    }
+
+    public static Bicycle createNewBicycle(){
+        System.out.println("Create your own custom bicycle!");
+        //display options to user to create custom bike
+
+        System.out.println("Let's start off by giving your custom bicycle a name");
+        input.nextLine();
+        String selectedName = input.nextLine();
+
+        System.out.println("Select the type of bike:");
+        printTypes(types);
+        int choice1 = validateInput(2);
+        String selectedType = types[choice1 -1];
+
+
+        System.out.println("Select the frame material:");
+        printMaterials(choice1, materials);
+        int choice2 = validateInput(4);
+        String selectedMaterial = materials[choice2 - 1];
+
+        // assign the weight and top speeds based on the material
+        float bikeWeight = 0;
+        int topSpeed = 0;
+        switch (choice2) {
+            case 1 -> {
+                bikeWeight = 10.2f;
+                topSpeed = 45;
+            }
+            case 2 -> {
+                bikeWeight = 12.1f;
+                topSpeed = 40;
+            }
+            case 3 -> {
+                bikeWeight = 10.5f;
+                topSpeed = 45;
+            }
+            case 4 -> {
+                bikeWeight = 8.3f;
+                topSpeed = 53;
+            }
+        }
+
+
+        System.out.println("Select the colour:");
+        printColours(colours);
+        int choice3 = validateInput(5);
+        String selectedColour = colours[choice3 - 1];
+
+        System.out.println("Select the tyre diameter:");
+
+        if (choice1 ==1){
+            printTyreDiameters(roadTyres);
+        }
+        else {
+            printTyreDiameters(mountainTyres);
+        }
+        int choice4 = validateInput(3);
+
+        float selectedDiameter;
+        if (choice1 == 1){
+            selectedDiameter = roadTyres[choice4 - 1];
+        }
+        else{
+            selectedDiameter = mountainTyres[choice4 - 1];
+        }
+
+        System.out.println("Select the number of gears you want to have");
+        printGears(gears);
+        int choice5 = validateInput(4);
+        int selectedGear = gears[choice5 -1];
+
+
+
+        // calculate total price of the bike created
+        int totalPrice = addPrices(getPrices(choice1), getPrices(choice2), getPrices(choice3), getPrices(choice4), getPrices(choice5));
+
+        // use all selected attributes in bicycle constructor method to create a new Bicycle object
+        // add this new Bicycle object to the arrayList of stored Bicycles
+
+        return new Bicycle(selectedName, selectedType, selectedMaterial, selectedColour, bikeWeight, selectedDiameter, topSpeed, selectedGear, totalPrice);
+    }
+
+
+
 
 
 }

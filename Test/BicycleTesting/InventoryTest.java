8fb4458f64b1188718f64b75ed1f67c8993622ab
ukush <64285005+ukush@users.noreply.github.com>
2021-08-11 17:04:18 +0100

    Added test directory

---
+++ Test/BicycleTesting/InventoryTest.java
@@ -0,0 +1,11 @@
+package BicycleTesting;
+
+public class InventoryTest {
+    /**
+     * I will write tests for the following functions:
+     * Sort list using recursion
+     * Add to Bicycle to list
+     * Remove Bicycle from list
+     * Search List and return index
+     */
+}

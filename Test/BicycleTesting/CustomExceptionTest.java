f7a4e71602fc0ab66104d7d24c7fb9849c89a1ea
ukush <ukushi01@gmail.com>
2021-08-18 13:29:25 +0100

    created class for custom exception and test class

---
+++ Test/BicycleTesting/CustomExceptionTest.java
@@ -0,0 +1,26 @@
+package BicycleTesting;
+
+import Bicycle.Bicycle;
+import Bicycle.CustomException;
+import jdk.jfr.Description;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class CustomExceptionTest {
+
+    public static ArrayList<Bicycle> testList = new ArrayList<Bicycle>();
+
+    @Description("Add a ")
+    @BeforeClass
+    public static void oneTimeSetUp(){
+
+    }
+
+
+
+}
